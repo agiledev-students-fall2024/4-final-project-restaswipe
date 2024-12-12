7ee9ea31e4e36aa3286f428863796918863ca0f3
JahleelT <jlt9287@nyu.edu>
2024-12-12 02:29:14 -0500

    finished up minor tweaks to css

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 require("dotenv").config();
 
-const port = process.env.PORT ?? 8080;
+const port = process.env.PORT ?? 8000;
 
 // call a function to start listening to the port
 const listener = app.listen(port, function () {
